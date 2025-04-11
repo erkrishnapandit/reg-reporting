649d93ddd966ded9764e3246b4438c6626da647e
Krishna Pandit <krishnakumarji100@gmail.com>
2025-04-12 01:26:39 +0530

    pieChart added

--- src/App.jsx
+++ src/App.jsx
@@ -21,10 +21,10 @@ function App() {
           <div className='w-full md:w-1/2'>
             <div className='border rounded-[6px] p-5 mx-6 md:ms-6 my-6'><ReportOverview/></div>
             <div className='border rounded-[6px] p-5 mx-6 md:ms-6 my-6'><DataQualitySummary/></div>
-            <div className='border rounded-[6px] p-5 mx-6 md:ms-6 my-3'><ExcepttonKer/></div>
+            <div className='border rounded-[6px] p-5 mx-6 md:ms-6 my-6'><ExcepttonKer/></div>
           </div > 
           <div className='w-full md:w-1/2' >
-            <p className='border rounded-[6px] p-5 m-2 mx-6 md:me-6 my-3'><AIAnomalieCheck/></p>
+            <p className='border rounded-[6px] p-5 m-2 mx-6 md:me-6 my-6'><AIAnomalieCheck/></p>
             <p className='border rounded-[6px] p-5 m-2 mx-6 md:me-6 my-6'><UpcommingDeadline/></p>
             <p className='border rounded-[6px] p-5 m-2 mx-6 md:me-6 my-6'><ProcessLogs/></p>
           </div>
--- src/components/DataQualitySummary.jsx
+++ src/components/DataQualitySummary.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
+import { PieChart } from './PieChart';
 
 function DataQualitySummary(){
     return (
-       
-        <div>
-             <p className='border p-3 font-bold text-xl'>Data Quality Summary</p>
+        <>
+        <p>Data Quality Summary</p>
+        <div className='flex flex-col md:flex-row justify-between'>
+           <div className='w-full md:w-1/2'>
+             <PieChart/>
         </div>
+        <div className='w-full md:w-1/2 p-6 my-auto'>
+            <ul className="mt-2 text-xl">
+                <li className="text-gray-600">○ Valid</li>
+                <li className="text-gray-600">○ Invalid</li>
+                <li className="text-gray-600">○ Warning</li>
+            </ul>
+        <button className="mt-2 bg-blue-500  text-2xl text-white px-4 py-2 rounded">Re-validate</button>
+        </div>
+       </div>
+        </>
     )
 }
 
-export default DataQualitySummary;
+export default DataQualitySummary; 
